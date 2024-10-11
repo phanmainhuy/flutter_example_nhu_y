ac7582e88dc16c22d3685e9f673a818e9a670b9d
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2024-10-11 15:58:10 +0700

    fix: fix custom switch widget for FF

--- lib/custom/widget/lp_platform_v2/custom_switch_widget.dart
+++ lib/custom/widget/lp_platform_v2/custom_switch_widget.dart
@@ -25,51 +25,58 @@ class MyApp extends StatelessWidget {
 }
 
 class CustomSwitchWidget extends StatefulWidget {
-  final double width;
-  final double height;
+  const CustomSwitchWidget({
+    super.key,
+    this.width,
+    this.height,
+    this.initValue,
+    this.activeColor,
+    this.inactiveColor,
+    this.toggleSize,
+    this.borderRadius,
+    this.onToggle,
+  });
+
+  final double? width;
+  final double? height;
   final bool? initValue;
 
   final Color? activeColor;
   final Color? inactiveColor;
   final double? toggleSize;
   final double? borderRadius;
-
-  const CustomSwitchWidget({
-    super.key,
-    required this.width,
-    required this.height,
-    required this.initValue,
-    this.activeColor,
-    this.inactiveColor,
-    this.toggleSize,
-    this.borderRadius,
-  });
-
+  final Function(bool value)? onToggle;
   @override
   State<CustomSwitchWidget> createState() => _CustomSwitchWidgetState();
 }
 
 class _CustomSwitchWidgetState extends State<CustomSwitchWidget> {
+  late bool status;
+
+  @override
+  void initState() {
+    super.initState();
+    status = widget.initValue ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    bool status = widget.initValue ?? false;
-    return Center(
-      child: FlutterSwitch(
-        width: widget.width, // 40.0
-        height: widget.height, // 20.0
-        valueFontSize: 12.0,
-        toggleSize: widget.toggleSize ?? 18.0, // 18.0
-        value: status,
-        borderRadius: widget.borderRadius ?? 30.0, // 30.0
-        activeColor: widget.activeColor ?? Colors.green,
-        inactiveColor: widget.inactiveColor ?? Colors.grey,
-        padding: 1,
-        onToggle: (val) {
-          setState(() {
-            status = val;
-          });
-        },
-      ),
+    return FlutterSwitch(
+      width: widget.width ?? 40,
+      height: widget.height ?? 20,
+      valueFontSize: 12.0,
+      toggleSize: widget.toggleSize ?? 18.0,
+      value: status,
+      borderRadius: widget.borderRadius ?? 30.0,
+      activeColor: widget.activeColor ?? Colors.green,
+      inactiveColor: widget.inactiveColor ?? Colors.grey,
+      padding: 1,
+      onToggle: (val) {
+        setState(() {
+          status = val;
+          widget.onToggle?.call(val);
+        });
+      },
     );
   }
 }
