4048dbeb643a4bfa6b19ff7bb50924fc681dbd5b
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2024-10-15 15:53:02 +0700

    feat: add get currency list

---
+++ lib/custom/functions/lp_platform_v2/for_get_data/get_list_pair.dart
@@ -0,0 +1,111 @@
+import 'package:flutter_example_nhuy/model/currency_image_option_model_struct%20copy.dart';
+
+List<CurrencyImageOptionModelStruct>? getListPair() {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+
+  dynamic data = [
+    {
+      "id": '1',
+      "name": "THB_USDT",
+      "imagePath": "8s93ijpg0y7n/1.thailand-THB.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '2',
+      "name": "PHP_USDT",
+      "imagePath": "32cbpo0perwi/2.philippines-PHP.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '3',
+      "name": "MYR_USDT",
+      "imagePath": "djjn31322hxz/3.myanmar-MYR.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '4',
+      "name": "SGD_USDT",
+      "imagePath": "42femq00xqsv/4.singapore-SGD.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '5',
+      "name": "KHR_USDT",
+      "imagePath": "mvcjna9x2t8w/5.cambodia-KHR.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '6',
+      "name": "VND_USDT",
+      "imagePath": "ehaj67llhqfs/6.vietnam-VND.png",
+      "isSelected": true,
+      "isDisplay": false
+    },
+    {
+      "id": '7',
+      "name": "CNY_USDT",
+      "imagePath": "gobqrid7byr6/7.china-CNY.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '8',
+      "name": "HKD_USDT",
+      "imagePath": "246qdpq4hqaa/8.hongkong-HKD.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '9',
+      "name": "JPY_USDT",
+      "imagePath": "e1pq2s22ipu6/9.japan-JPY.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '10',
+      "name": "KRW_USDT",
+      "imagePath": "mc4dazpv0snx/10.korea-KRW.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '11',
+      "name": "IDR_USDT",
+      "imagePath": "xfwy6xbuk1aa/11.indonesia-IDR.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '12',
+      "name": "CAD_USDT",
+      "imagePath": "thdm4jhufh3s/12.canada-CAD.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '13',
+      "name": "AUD_USDT",
+      "imagePath": "26ogmrtk8zlm/13.australia-AUD.png",
+      "isSelected": false,
+      "isDisplay": false
+    },
+    {
+      "id": '14',
+      "name": "SAR_USDT",
+      "imagePath": "0xly0pcqix8p/14.macao-SAR.png",
+      "isSelected": false,
+      "isDisplay": false
+    }
+  ];
+  final listPair =
+      data.map((item) => CurrencyImageOptionModelStruct.fromMap(item)).toList();
+  return listPair;
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
---
+++ lib/model/currency_image_option_model_struct.dart
@@ -0,0 +1,83 @@
+class CurrencyImageOptionModelStruct {
+  CurrencyImageOptionModelStruct({
+    String? id,
+    String? name,
+    String? imagePath,
+    bool? isSelected,
+    bool? isDisplay,
+  })  : _id = id,
+        _name = name,
+        _imagePath = imagePath,
+        _isSelected = isSelected,
+        _isDisplay = isDisplay;
+
+  // "id" field.
+  String? _id;
+  String get id => _id ?? '';
+  set id(String? val) => _id = val;
+
+  bool hasId() => _id != null;
+
+  // "name" field.
+  String? _name;
+  String get name => _name ?? '';
+  set name(String? val) => _name = val;
+
+  bool hasname() => _name != null;
+
+  // "imagePath" field.
+  String? _imagePath;
+  String get imagePath => _imagePath ?? '';
+  set imagePath(String? val) => _imagePath = val;
+
+  bool hasImagePath() => _imagePath != null;
+
+  // "isSelected" field.
+  bool? _isSelected;
+  bool get isSelected => _isSelected ?? false;
+  set isSelected(bool? val) => _isSelected = val;
+
+  bool hasIsSelected() => _isSelected != null;
+
+  // "isDisplay" field.
+  bool? _isDisplay;
+  bool get isDisplay => _isDisplay ?? false;
+  set isDisplay(bool? val) => _isDisplay = val;
+
+  bool hasisDisplay() => _isDisplay != null;
+
+  static CurrencyImageOptionModelStruct fromMap(Map<String, dynamic> data) =>
+      CurrencyImageOptionModelStruct(
+        id: data['id'] as String?,
+        name: data['name'] as String?,
+        imagePath: data['imagePath'] as String?,
+        isSelected: data['isSelected'] as bool?,
+        isDisplay: data['isDisplay'] as bool?,
+      );
+
+  static CurrencyImageOptionModelStruct? maybeFromMap(dynamic data) =>
+      data is Map
+          ? CurrencyImageOptionModelStruct.fromMap(data.cast<String, dynamic>())
+          : null;
+
+  Map<String, dynamic> toMap() => {
+        'id': _id,
+        'name': _name,
+        'imagePath': _imagePath,
+        'isSelected': _isSelected,
+        'isDisplay': _isDisplay,
+      };
+
+  @override
+  String toString() => 'CurrencyImageOptionModelStruct(${toMap()})';
+
+  @override
+  bool operator ==(Object other) {
+    return other is CurrencyImageOptionModelStruct &&
+        id == other.id &&
+        name == other.name &&
+        imagePath == other.imagePath &&
+        isSelected == other.isSelected &&
+        isDisplay == other.isDisplay;
+  }
+}
