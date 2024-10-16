d098871b851160907f2d3479ebe14091820776f0
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2024-10-16 13:26:07 +0700

    fix: fix divisionModulo function

--- lib/custom/functions/lp_platform_v2/division_modulo.dart
+++ lib/custom/functions/lp_platform_v2/division_modulo.dart
@@ -1,10 +1,16 @@
 int? divisionModulo(
-  int divisor,
-  int dividend,
+  int? divisor,
+  int? dividend,
 ) {
   /// MODIFY CODE ONLY BELOW THIS LINE
 
   try {
+    if (dividend == null || divisor == null) {
+      return 0;
+    }
+    if (dividend == 0) {
+      return 0;
+    }
     return divisor % dividend;
   } catch (e) {
     return 0;
