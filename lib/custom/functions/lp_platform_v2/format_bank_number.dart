74c5c40b8bb9bb4850520d85e86485bdca52fa79
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2024-10-21 15:53:16 +0700

    fix: edit formatBankNumber with hideBank

--- lib/custom/functions/lp_platform_v2/format_bank_number.dart
+++ lib/custom/functions/lp_platform_v2/format_bank_number.dart
@@ -1,6 +1,6 @@
 void main() {
   String number = '1234567890123456789012312312';
-  String formatted = formatBankNumber(number, 4, '-');
+  String formatted = formatBankNumber(number, 4, '', true);
   print(formatted); // Output: 1234 - 5678 - 9012 - 3456 - 7890
 }
 
@@ -8,6 +8,7 @@ String formatBankNumber(
   String? input,
   int? separateNumber,
   String? symbol,
+  bool? hideBank,
 ) {
   /// MODIFY CODE ONLY BELOW THIS LINE
   if (input == null || input == '') {
@@ -22,14 +23,33 @@ String formatBankNumber(
     symbol = '';
   }
 
+  hideBank ??= false;
+
   // format String
   String numStr = input.toString();
-
   List<String> parts = [];
+
   try {
     for (int i = 0; i < numStr.length; i += separateNumber) {
-      int end = (i + 4 < numStr.length) ? i + separateNumber : numStr.length;
-      parts.add(numStr.substring(i, end));
+      int end = (i + separateNumber < numStr.length)
+          ? i + separateNumber
+          : numStr.length;
+      String part = numStr.substring(i, end);
+
+      // Check if hiding numb
+      if (hideBank == true && i + separateNumber < numStr.length) {
+        parts.add('*' * part.length);
+      } else {
+        parts.add(part);
+      }
+    }
+
+    if (hideBank) {
+      return parts.join(' ');
+    }
+
+    if (symbol == '') {
+      return parts.join(' ');
     }
 
     return parts.join(' $symbol ');
