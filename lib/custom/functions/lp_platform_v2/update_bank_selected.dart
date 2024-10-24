544592de3a5fdf43936e0fb0ea1cf0c2d1b2b23f
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2024-10-24 10:15:01 +0700

    feat: add updateBankList function for sell

--- lib/custom/functions/lp_platform_v2/update_bank_selected.dart
+++ lib/custom/functions/lp_platform_v2/update_bank_selected.dart
@@ -1,9 +1,38 @@
 import 'package:flutter_example_nhuy/model/bank_filter_data_model_struct.dart';
 
-List<BankFilterDataModelStruct> updateBankSelected() {
+List<BankFilterDataModelStruct> updateBankSelected(
+    List<BankFilterDataModelStruct>? oldBankList, String? bankUuid) {
   /// MODIFY CODE ONLY BELOW THIS LINE
 
-  return [];
+  try {
+    if (oldBankList == null || oldBankList == []) {
+      return [];
+    }
+    if (bankUuid == null || bankUuid == '') {
+      return oldBankList;
+    }
+    List<BankFilterDataModelStruct> updatedList = oldBankList.map((bank) {
+      if (bank.uuid == bankUuid) {
+        bank.isSelected = true;
+        return bank;
+      } else {
+        if (bank.isSelected == true) {
+          bank.isSelected = false;
+          return bank;
+        }
+      }
+      return bank;
+    }).toList();
+
+    return updatedList;
+  } catch (e) {
+    print('ERROR updateBankSelected: ${e.toString()}');
+    if (oldBankList == null || oldBankList == []) {
+      return [];
+    } else {
+      return oldBankList;
+    }
+  }
 
   /// MODIFY CODE ONLY ABOVE THIS LINE
 }
