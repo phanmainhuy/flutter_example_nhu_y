a7c8ac404438d0582b8a70d34cdacf456e7210cb
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2024-10-25 01:15:40 +0700

    feat: add checkListStringContainsString funtion

---
+++ lib/custom/functions/lp_platform_v2/check_list_string_contains_string.dart
@@ -0,0 +1,25 @@
+void main() {
+  print(checkListStringContainsString('VN', ['VN', 'TH']));
+}
+
+bool checkListStringContainsString(
+  String? inputString,
+  List<String>? listString,
+) {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+  try {
+    if (inputString == '' ||
+        inputString == null ||
+        listString == [] ||
+        listString == null) {
+      return false;
+    } else {
+      return listString.contains(inputString);
+    }
+  } catch (e) {
+    print("ERROR checkListStringContainsString: ${e.toString()}");
+    return false;
+  }
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
