1c284b51f3f5932f8e8924606294e16eaa5e93ef
phanmainhuy <68746922+phanmainhuy@users.noreply.github.com>
2024-10-30 14:49:00 +0700

    feat: add handleCountryList function for selected country bottomsheet

---
+++ lib/custom/functions/lp_platform_v2/for_get_data/country_currencies/handle_country_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_example_nhuy/model/country_image_option_model_struct.dart';
+
+List<CountryImageOptionModelStruct> handleCountryList(
+  List<CountryImageOptionModelStruct> countryList,
+  String idSelected,
+  bool isMultiOptions,
+) {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+
+  List<CountryImageOptionModelStruct> countryListNew = countryList;
+
+  // multi selection
+  if (countryList == []) {
+    return [];
+  } else if (isMultiOptions) {
+    for (int i = 0; i < countryListNew.length; i++) {
+      if (countryListNew[i].id == idSelected) {
+        countryListNew[i].isSelected = true;
+      }
+    }
+    return countryListNew;
+  } else {
+    try {
+      for (int i = 0; i < countryListNew.length; i++) {
+        if (countryListNew[i].isDisableSelected == true) {
+          countryListNew[i].isSelected = false;
+        }
+        if (countryListNew[i].isSelected == true &&
+            countryListNew[i].id != idSelected) {
+          countryListNew[i].isSelected = false;
+        } else if (countryListNew[i].id == idSelected) {
+          countryListNew[i].isSelected = true;
+        }
+      }
+      return countryListNew;
+    } catch (e) {
+      print("ERROR handleCountryList: ${e.toString()}");
+      return countryList;
+    }
+  }
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
